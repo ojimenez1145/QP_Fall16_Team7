4eab211440ce626246ed295d1596d1eedc39c6fc
ojimenez1145 <o4jimene@ucsd.edu>
2016-11-20 19:31:07 -0800

    Create GLOBAL.php

---
+++ GLOBAL.php
@@ -0,0 +1,21 @@
+<?php
+  date_default_timezone.set('America/Los_Angeles');
+  
+  include "../navbar.html";
+?>
+
+/*Image to be used as header for website*/
+  <div class="header">
+    <br><br>
+    <img src="Danger_Banner.jpg" alt="logo" />
+    
+<?php
+  include "navabar.html";
+    echo "</dib>";
+    
+    function connectMongo(){
+      $connection = new MoncoClient("mongodb://mongodb://admin:admin@ds019746.mlab.com:19746/qpfallteam7");
+      $database = $connection->qpfallteam7;
+      return $database;
+    } 
+?>
